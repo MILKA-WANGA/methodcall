22b0e0eabf21900e582a3918bcd93ff23158fee3
MILKA-WANGA <83470011+MILKA-WANGA@users.noreply.github.com>
2023-04-03 02:00:08 -0700

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student
+{
+    String studentName;
+    int studentAge;
+    int rollNumber;
+    void display()
+    {
+        System.out.println(studentName);
+         System.out.println(studentAge);
+          System.out.println(rollNumber);
+    }
+}
---
+++ StudentDemo.java
@@ -0,0 +1,11 @@
+public class StudentDemo
+{
+public static void main(String[]args)
+{
+    Student s = new Student();
+    
+    s.display();
+    
+
+}
+}
